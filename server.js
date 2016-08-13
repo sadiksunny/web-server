de2da30248df185bba04f6dd247d926018989912
Sadik Mahmud Sunny <sadik.m.sunny@gmail.com>
2016-08-12 21:26:25 -0400

    Update port for heroku

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
 var express = require ('express');
-var app = express();
-var PORT = 3000;
+var app = express ();
+var PORT = process.env.PORT || 3000;
 // app.get('/', function (req , res){
 	// res.send ('Hello Express');
 // });
 var middleware = require ('./middleware.js');
-var middleware = {
-	requireAuthentication: function (req , res , next){
-		console.log ('private route hit!');
-		next();
-	},
-	logger: function (req , res , next){
-		
-	console.log ('Request : ' + new Date().toString() + req.method + ' ' + req.originalUrl);
-next();	
-	}
-};
+
 app.use (middleware.logger);
  app.use(middleware.requireAuthentication);
 
